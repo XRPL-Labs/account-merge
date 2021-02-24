649c375e5254ee5b0b96ba6b754ddb122dcdf889
KoenPaas <koen@paas.nl>
2021-02-24 14:48:53 +0100

    Delete comment

--- src/components/Start.vue
+++ src/components/Start.vue
@@ -1,9 +1,4 @@
 <template>
-    <!-- <h1 class="h3 font-weight-bold">{{ $t("start.title") }}</h1>
-    <h6>
-        <b class="text-primary">{{ $t("start.subtitle") }}</b>
-    </h6>
-    <hr> -->
     <Header />
     <div>
         <p>{{ $t("start.description") }}</p>
